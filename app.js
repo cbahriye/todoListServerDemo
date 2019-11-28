a45da04cbff877b0082f9c09824b742011aeb322
xuanli-liu <ethan1993liu@gmail.com>
2019-11-27 20:17:35 -0500

    create todo list varaible and make it global

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let todos = [
+    {id: "1", task: 'task1', isCompleted: false},
+    {id: "2", task: 'task2', isCompleted: false},
+    {id: "3", task: 'task3', isCompleted: false},
+    {id: "4", task: 'task4', isCompleted: false},
+];
+
 // we handle the get request using app.get
 app.get('/', (req, res) => res.send('Welcome home'));
 
@@ -9,7 +16,7 @@ app.get('/', (req, res) => res.send('Welcome home'));
 // /api/todos
 app.get('/api/todos', (req, res) => {
 
-    res.send(["a", "b", "c"]);
+    res.send(todos);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
