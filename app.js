ed862374e07ce6ca03f172b3ea368a2eca50943b
xuanli-liu <ethan1993liu@gmail.com>
2019-11-27 20:14:16 -0500

    create endpiont for all todos

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const port = 3000;
 // we handle the get request using app.get
 app.get('/', (req, res) => res.send('Welcome home'));
 
+// we want an endpoint getting all todos
+// /api/todos
+app.get('/api/todos', (req, res) => {
+
+    res.send(["a", "b", "c"]);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
