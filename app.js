07081f2c8df7613978ee026851b5856bca8e5940
xuanli-liu <ethan1993liu@gmail.com>
2019-11-27 20:07:58 -0500

    start hello world express

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// we handle the get request using app.get
+app.get('/', (req, res) => res.send('Welcome home'));
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
