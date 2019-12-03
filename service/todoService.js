adb971c0679228fc22cc814867fa149aa570c2bd
xuanli-liu <ethan1993liu@gmail.com>
2019-12-02 19:50:20 -0500

    use class to refactor ToDo service

--- service/todoService.js
+++ service/todoService.js
@@ -1,47 +1,50 @@
-let todos = [
-    {id: "1", task: 'task1', isCompleted: false},
-    {id: "2", task: 'task2', isCompleted: false},
-    {id: "3", task: 'task3', isCompleted: false},
-    {id: "4", task: 'task4', isCompleted: false},
-];
-let todosId = todos.length;
-
-function getAll() {
-    return todos;
-}
-
-function findOne(id) {
-    return todos.find(x => x.id === id);
-}
-
-function findByName(name) {
-    return todos.filter(
-        x => x.task.indexOf(name) !== -1);
-}
-
-function add(item) {
-    const itemToAdd = {...item, id: ++todosId, isCompleted: item.isCompleted || false};
-    todos.push(itemToAdd);
-    return itemToAdd;
-}
-
-
-function update(id, fields) {
-    const itemToupdate = todos.find(x => x.id === id);
-    for (let key of Object.keys(fields)) {
-        itemToupdate[key] = fields[key];
+class ToDo {
+    constructor() {
+        this.todos = [
+            {id: "1", task: 'task1', isCompleted: false},
+            {id: "2", task: 'task2', isCompleted: false},
+            {id: "3", task: 'task3', isCompleted: false},
+            {id: "4", task: 'task4', isCompleted: false},
+        ];
+        this.todosId = this.todos.length;
     }
 
-    return itemToupdate;
+    getAll() {
+        return this.todos;
+    }
+
+    findOne(id) {
+        return this.todos.find(x => x.id === id);
+    }
+
+    findByName(name) {
+        return this.todos.filter(
+            x => x.task.indexOf(name) !== -1);
+    }
+
+    add(item) {
+        const itemToAdd = {...item, id: ++this.todosId, isCompleted: item.isCompleted || false};
+        this.todos.push(itemToAdd);
+        return itemToAdd;
+    }
+
+
+    update(id, fields) {
+        const itemToupdate = this.todos.find(x => x.id === id);
+        for (let key of Object.keys(fields)) {
+            itemToupdate[key] = fields[key];
+        }
+
+        return itemToupdate;
+    }
+
+    remove(id) {
+        const itemToDelete = this.todos.find(x => x.id === id);
+        this.todos.splice(this.todos.indexOf(itemToDelete), 1);
+
+        return itemToDelete;
+    }
 }
 
-function remove(id) {
-    const itemToDelete = todos.find(x => x.id === id);
-    todos.splice(todos.indexOf(itemToDelete), 1);
 
-    return itemToDelete;
-}
-
-module.exports = {
-    getAll, findOne, findByName, add, remove, update
-};
+module.exports = new ToDo();
